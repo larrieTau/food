ab44f783b683a9de9781b5c662b67d187aedeba0
Larrie-Tau <kasibantelarry@gmail.com>
2025-06-12 17:18:35 +0300

    firebase intergration

--- food_1/android/app/build.gradle.kts
+++ food_1/android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("dev.flutter.flutter-gradle-plugin")
 }
 
